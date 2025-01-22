a65268356dc4d01f6788742583fa8b522686df92
Julian Rakow <julian.rakow@yahoo.com>
2025-01-22 16:30:40 +0100

    add assets and StartScreen (needs more work)

---
+++ lib/start_screen.dart
@@ -0,0 +1,81 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/backgrounds/bg_mainscreen.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 200,
+            left: 30,
+            child:
+                Image(image: AssetImage("assets/graphics/cupcake_chick.png")),
+          ),
+          Positioned(
+            bottom: 100,
+            child: ClipRRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      border: Border.all(color: Colors.white)),
+                  width: 400,
+                  height: 200,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Feeling Snackish Today?",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25),
+                        ),
+                        Text(
+                          "Explore Angi's most popular snack selection and get instantly happy.",
+                          style: TextStyle(color: Colors.grey),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        FilledButton(
+                          style: FilledButton.styleFrom(
+                              backgroundColor: Colors.pink,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10))),
+                          onPressed: () {},
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.only(left: 32.0, right: 32.0),
+                            child: Text("Order Now"),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}

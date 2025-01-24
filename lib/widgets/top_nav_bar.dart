09c190b2e77db9a844684d7262d1fb7de3b3356f
Julian Rakow <julian.rakow@yahoo.com>
2025-01-24 15:51:42 +0100

    more work on topnavbar (needs refactoring)

--- lib/widgets/top_nav_bar.dart
+++ lib/widgets/top_nav_bar.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class TopNavBar extends StatelessWidget {
@@ -13,66 +15,109 @@ class TopNavBar extends StatelessWidget {
         spacing: 10,
         children: [
           ClipRRect(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                border: Border.all(color: Colors.white),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.info,
-                      color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minWidth: 90),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(64, 255, 255, 255),
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.white, width: 1),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      spacing: 5,
+                      children: [
+                        Icon(
+                          Icons.fastfood,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          "All categories",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Icon(
+                          Icons.keyboard_arrow_down,
+                          color: Colors.white,
+                        )
+                      ],
                     ),
-                    Text("All categories"),
-                    Icon(Icons.keyboard_arrow_down)
-                  ],
+                  ),
                 ),
               ),
             ),
           ),
-          Container(
-            width: 90,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(color: Colors.white),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "Salty",
-                textAlign: TextAlign.center,
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minWidth: 90),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.white),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Salty",
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
               ),
             ),
           ),
-          Container(
-            width: 90,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(color: Colors.white),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "Sweet",
-                textAlign: TextAlign.center,
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minWidth: 90),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(64, 255, 255, 255),
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.white, width: 1),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Sweet",
+                      style: TextStyle(color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
               ),
             ),
           ),
-          Container(
-            width: 90,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(color: Colors.white),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "Drinks",
-                textAlign: TextAlign.center,
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minWidth: 90),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(64, 255, 255, 255),
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.white, width: 1),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Drinks",
+                      style: TextStyle(color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
               ),
             ),
           ),
